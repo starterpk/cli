931c5b3d5e2d6d761d57b49b0be941e2828f55d9
Sunil Sandhu <sunil.sandhu87@gmail.com>
2020-11-14 19:55:38 +0100

    wip

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const inquirer = require("inquirer");
 const figlet = require("figlet");
 const chalk = require("chalk");
 const clone = require("git-clone");
-
-console.log(
-  chalk.yellow.bgBlack(
-    figlet.textSync("Starter Pack", { horizontalLayout: "fitted", font: "Standard" })
-  )
-);
+const { exec } = require("child_process");
 
 const promptQuestions = [
   {
@@ -22,30 +17,119 @@ const promptQuestions = [
     type: "list",
     name: "pack",
     message: "Which Starter Pack would you like to use?",
-    choices: ["React", "React + Express"],
+    choices: [
+      "Angular",
+      "React",
+      "React (Next.js)",
+      "React + Express",
+      "React + TypeScript",
+      "Svelte",
+      "Vue",
+      "Vue + TypeScript",
+      "Website",
+    ],
   },
 ];
+
+function execResponse(error, stdout, stderr) {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    console.log(
+      chalk.green("Your Starter Pack was unable to create your project. " + error.message)
+    );
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    console.log(chalk.green("Your Starter Pack has been created. Have fun building stuff!"));
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(chalk.green("Your Starter Pack has been created. Have fun building stuff!"));
+  return;
+}
+
+function cloneRepo(pack, dirName) {
+  switch (pack) {
+    case "Angular":
+      console.log(chalk.blue("Creating Pack, please wait..."));
+      exec("npx -p @angular/cli ng new " + dirName + " --defaults=true", (error, stdout, stderr) =>
+        execResponse(error, stdout, stderr)
+      );
+      break;
+    case "React":
+      console.log(chalk.blue("Creating Pack, please wait..."));
+      exec("npx create-react-app " + dirName, (error, stdout, stderr) =>
+        execResponse(error, stdout, stderr)
+      );
+      break;
+    case "React (Next.js)":
+      console.log(chalk.blue("Creating Pack..."));
+      exec("npx create-next-app " + dirName, (error, stdout, stderr) =>
+        execResponse(error, stdout, stderr)
+      );
+      break;
+    case "React + Express":
+      console.log(chalk.blue("Creating Pack..."));
+      clone("https://github.com/starterpk/pack-react-express", "./" + dirName);
+      break;
+    case "React + TypeScript":
+      console.log(chalk.blue("Creating Pack, please wait..."));
+      exec(
+        "npx create-react-app " + dirName + " --template typescript",
+        (error, stdout, stderr) => execResponse(error, stdout, stderr)
+        // clone("https://github.com/starterpk/pack-react", "./" + dirName);
+      );
+      break;
+    case "Svelte":
+      console.log(chalk.blue("Creating Pack, please wait..."));
+      exec(
+        "npx degit 'sveltejs/template' " + dirName + " && cd " + dirName + " && npm install",
+        (error, stdout, stderr) => execResponse(error, stdout, stderr)
+      );
+      break;
+    case "Vue":
+      console.log(chalk.blue("Creating Pack, please wait..."));
+      exec("npx vue create " + dirName + " -d", (error, stdout, stderr) =>
+        execResponse(error, stdout, stderr)
+      );
+      break;
+    case "Vue + TypeScript":
+      console.log(chalk.blue("Creating Pack, please wait..."));
+      exec(
+        "npx vue create " + dirName + " -d && cd " + dirName + " && npx vue add typescript -d",
+        (error, stdout, stderr) => execResponse(error, stdout, stderr)
+      );
+      break;
+    case "Website":
+      console.log(chalk.blue("Creating Pack..."));
+      clone("https://github.com/starterpk/pack-website", "./" + dirName);
+      break;
+    default:
+      console.log("You didn't pick anything...");
+  }
+}
+
+// CLI output begins here
+
+console.log(
+  chalk.yellow.bgBlack(
+    figlet.textSync("Starter Pack", {
+      horizontalLayout: "fitted",
+      font: "Standard",
+    })
+  )
+);
 
 inquirer
   .prompt(promptQuestions)
   .then((answer) => {
     const cleanAppName = answer.name.replace(/\s+/g, "-").toLowerCase();
-    switch (answer.pack) {
-      case "React":
-        console.log(chalk.blue("Creating Pack..."));
-        clone("https://github.com/starterpk/pack-react", "./" + cleanAppName);
-        break;
-      case "React + Express":
-        console.log(chalk.blue("Creating Pack..."));
-        clone("https://github.com/starterpk/pack-react-express", "./" + cleanAppName);
-        break;
-      default:
-        console.log("You didn't pick anything...");
-    }
+    cloneRepo(answer.pack, cleanAppName);
   })
-  .then(() => {
-    console.log(chalk.green("Your Starter Pack has been created. Have fun building stuff!"));
-  })
+  // .then(() => {
+  //   console.log(chalk.green("Your Starter Pack has been created. Have fun building stuff!"));
+  // })
   .catch((error) => {
     if (error.isTtyError) {
       console.log("Prompt couldn't be rendered in the current environment");
